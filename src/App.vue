1a4e3eaef34d627e5df4f80b32a002cfafb7e643
moumitasub <moumitasub@gmail.com>
2023-07-18 14:02:10 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import Features from './components/Features.vue';
+import Functionalities from './components/Functionalities.vue';
+import Header from './components/Header.vue';
+import Services from './components/Services.vue';
+import Testimonial from './components/Testimonial.vue';
+import Gallery from './components/Gallery.vue';
+import Pricing from './components/Pricing.vue';
+import Faq from './components/Faq.vue';
+import Subfooter from './components/Subfooter.vue';
+import Footer from './components/Footer.vue';
+
+
+
+</script>
+
+<template>
+    <Header/>
+    <Features/>
+    <Functionalities/>
+    <Services/>
+    <Testimonial/>
+    <Gallery/>
+    <Pricing/>
+    <Faq/>
+    <Subfooter/>
+    <Footer/>
+  
+</template>
+
+<style scoped>
+</style>
